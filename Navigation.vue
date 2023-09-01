5a9a0c7edb1ab302ad2ac0065303fffbaf8d1b0f
Francesco <fcfrancescocrupi@gmail.com>
2023-09-01 17:55:26 +0200

    Adding css theme color

--- Navigation.vue
+++ Navigation.vue
@@ -22,15 +22,21 @@
 </script>
 
 <template>
-    <button class="button_menu"  @click="NavigationSchema.show = true">Menu</button>
+    <button v-if="!NavigationSchema.show" class="button_menu"  @click="NavigationSchema.show = true">
+        <svg viewBox="0 0 60 60">
+            <path id="menu_line-1" stroke="var(--color)" stroke-width="1.5" d="M14 18 L46 18" ></path>
+            <path id="menu_line-2" stroke="var(--color)" stroke-width="1.5" d="M10 31 L50 31" ></path>
+            <path id="menu_line-3" stroke="var(--color)" stroke-width="1.5" d="M14 44 L46 44" ></path>
+        </svg>
+    </button>
 <div v-if="NavigationSchema.show">
 
-    <div class="navigation">
+    <div class="navigation bounce_up">
         <div class="nav_header">
             <select v-model="NavigationSchema.selected">
                 <option v-for="value in NavigationSchema.items">{{ value.label }}</option>
             </select>
-            <button @click="NavigationSchema.show = false">X</button>
+            <button class="btn_close" @click="NavigationSchema.show = false">X</button>
         </div>
         <div>
             <div v-if="items?.type === 'group' " class="nav_items_container">
@@ -39,9 +45,11 @@
                 </div>
             </div>
             <div v-if="items?.type === 'subgroup' " class="nav_items_container">
-                <select v-model="subSelect">
-                    <option v-for="o in items.subgroup">{{ o.label }}</option>
-                </select>
+                <div class="select">
+                    <select v-model="subSelect">
+                        <option v-for="o in items.subgroup">{{ o.label }}</option>
+                    </select>
+                </div>
                 <div v-for="item in items.subgroup" class="nav_items_container">
                     <div v-if=" item.label === subSelect" v-for="sub in item.group">
                         <NavItemBuilder :item="sub"></NavItemBuilder>
@@ -60,7 +68,10 @@
         position: fixed;
         left: 0px;
         bottom: 0px;
-        background: rgb(215, 250, 255);
+        background: var(--background);
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+        box-shadow: 0px -1px 3px 1px rgba(255, 255, 255, 0.172);
     }
 
     .nav_header{
@@ -69,19 +80,97 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding-left: 5px;
-        padding-right: 5px;
+        padding-left: 15px;
+        padding-right: 15px;
+        border-bottom: solid 1px var(--border);
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;        
     }
 
     .nav_items_container{
         display: flex;
-
+        padding: 15px;
     }
 
     .button_menu{
         position: fixed;
-        width: 10vw;
-        left: 45vw;
+        width: 5vw;
+        height: 5vw;
+        left: 47.5vw;
         bottom: 10px;
+        background: rgba(0, 0, 0, 0.069);
+        color: var(--color);
+        border: none;
+        border-radius: 5vw;
+        box-shadow: 0px 0px 5px 1px rgba(255, 255, 255, 0.062) inset;
+    }
+
+    .btn_close{
+        border: none;
+        background: transparent;
+        color: var(--color);
+        height: 4.5vh;
+        width: 4.5vh;
+        border-radius: 3px;
+        font-size: 2rem;
+        overflow: hidden;
+        margin-bottom: 1vh;
+    }
+
+    .bounce_up{
+        animation: bounce_up 0.5s ease;
+    }
+
+    button:hover{
+        cursor: pointer;
+    }
+
+    select{
+        width: 20vw;
+        height: 4.5vh;
+        border-radius: 5px;
+        font-size: 1.2rem;
+        background: rgba(255, 255, 255, 0.061);
+        color: var(--color);
+        border: solid 1px var(--border);
+    }
+
+    .select{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: start;
+    }    
+
+    @keyframes bounce_up {
+        from{
+            transform: translateY(100%);
+        }
+        to{
+            transform: translateY(0%);
+        }
+    }
+
+    #menu_line-1{
+        animation: line_menu 0.4s ease-in;
+    }
+    #menu_line-2{
+        animation: line_menu 0.4s ease-in;
+        animation-delay: 0.1s;
+    }
+    #menu_line-3{
+        animation: line_menu 0.4s ease-in;
+        animation-delay: 0.2s;
+    }
+
+    @keyframes line_menu {
+        0%{
+            stroke-dasharray: 30;
+            stroke-width: 3;
+        }
+        99%{
+            stroke-dasharray: 0;
+            stroke-width: 0;
+        }
     }
 </style>
--- components/NavItemBuilder.vue
+++ components/NavItemBuilder.vue
@@ -22,7 +22,7 @@
     <div v-if="item.type === 'event'">
         <button @click="item.event();">{{ item.label }}</button>
     </div>    
-    <div v-if="item.type === 'eventselection'">
+    <div v-if="item.type === 'eventselection'" class="select">
         {{ item.label }}
         <select v-model="item.data" @change="activeEventSelect()">
             <option v-for="option in item.options">{{option.label}}</option>
@@ -30,4 +30,44 @@
     </div>
 </template>
 <style scoped>
+
+    *{
+        color: var(--color);
+    }
+
+    div, button{
+        width: 15vw;
+        height: 15vh;
+        border: solid 1px var(--border);
+        border-radius: 10px;
+        margin-right: 1vw;
+        background-color: rgba(0, 0, 0, 0.101);
+    }
+
+    button:hover{
+        cursor: pointer;
+    }
+
+    button{
+        /* background: var(--background); */
+        border: none;
+        font-size: 1rem;
+    }
+
+    select{
+        width: 15vw;
+        height: 4.5vh;
+        border-radius: 5px;
+        font-size: 1.2rem;
+        background: transparent;
+        color: var(--color);
+        border: none;
+    }
+
+    .select{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: start;
+    }
 </style>
